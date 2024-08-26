127eea3cf93ac4b64e567fcab696e687239ec045
Kebechet <samosidor@gmail.com>
2024-08-26 09:42:33 +0200

    Add project files.

---
+++ BuildError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BuildError/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildError
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
